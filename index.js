4869c25fb93509a9fb639bded7040d55c0b05f73
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-08-25 14:49:56 +0530

    invoice reminder added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { support } from "./EmailTemplates/support.js";
 import { quotes } from "./EmailTemplates/quotes.js";
 import { verification } from "./EmailTemplates/verification.js";
 import { otp } from "./EmailTemplates/otp.js";
+import { invoiceReminder } from "./EmailTemplates/invoiceReminder.js";
 
 dotenv.config();
 
@@ -14,104 +15,132 @@ const resendApiKey = process.env.RESEND_API_KEY;
 
 const resend = new Resend(resendApiKey);
 
+const inviteParams = {
+  signUpLink: "https://fdev.soleapp.com.au/",
+  unsubscribeLink: "https://fdev.soleapp.com.au/",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+};
+
+const howToUseParams = {
+  userName: "Rahul Joshi",
+  regardsBy: "Johann Oberholzer",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+  knowledgeBaseLink: "https://fdev.soleapp.com.au/",
+};
+
+const trialOverParams = {
+  userName: "Rahul Joshi",
+  trialEndDate: "25/08/2025",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+  bootcampLink: "https://fdev.soleapp.com.au/",
+  supportHubLink: "https://fdev.soleapp.com.au/",
+};
+
+const supportParams = {
+  userName: "Ravi Joshi",
+  supportEmail: "test@gmail.com",
+  supportEndDate: "25/08/2025",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+  loginLink: "https://fdev.soleapp.com.au/",
+  regardsBy: "Neeraj Verma",
+};
+
+const quoteParams = {
+  userName: "Ashish Rautela",
+  quoteAmt: "AUD $225.00",
+  quoteId: "22222222",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+};
+
+const verificationParams = {
+  userName: "Ravi Joshi",
+  verifyAccountLink: "https://fdev.soleapp.com.au/",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+};
+
+const otpParams = {
+  userName: "Ashish Rautela",
+  otp: "123456",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+};
+
+const invoiceReminderParams = {
+  userName: "Ashish Rautela",
+  invoiceNo: "INV-123456",
+  invoiceAmt: "AUD $225.00",
+  invoiceFrom: "Rahul Joshi",
+  dueDate: "01 Aug 2025",
+  bsbNo: "001122",
+  accNo: "XXX1234",
+  regardsBy: "Rahul Joshi",
+  twitterLink: "https://x.com/home",
+  linkedinLink: "https://www.linkedin.com/",
+  facebookLink: "https://www.facebook.com/",
+  helpcenterLink: "https://fdev.soleapp.com.au/",
+  privacyLink: "https://fdev.soleapp.com.au/",
+  termsLink: "https://fdev.soleapp.com.au/",
+  appStoreLink: "https://www.apple.com/in/app-store/",
+  playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
+};
+
+const pdfBase64 = async () => {
+  const response = await fetch(
+    "https://morth.nic.in/sites/default/files/dd12-13_0.pdf"
+  );
+  const pdfBuffer = await response.arrayBuffer();
+  const convertedBase64 = Buffer.from(pdfBuffer).toString("base64");
+  return convertedBase64;
+};
+
 export const sendCompanyCreationEmail = async (adminEmail) => {
   try {
-    const inviteParams = {
-      signUpLink: "https://fdev.soleapp.com.au/",
-      unsubscribeLink: "https://fdev.soleapp.com.au/",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-    };
-
-    const howToUseParams = {
-      userName: "Rahul Joshi",
-      regardsBy: "Johann Oberholzer",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-      appStoreLink: "https://www.apple.com/in/app-store/",
-      playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
-      knowledgeBaseLink: "https://fdev.soleapp.com.au/",
-    };
-
-    const trialOverParams = {
-      userName: "Rahul Joshi",
-      trialEndDate: "25/08/2025",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-      appStoreLink: "https://www.apple.com/in/app-store/",
-      playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
-      bootcampLink: "https://fdev.soleapp.com.au/",
-      supportHubLink: "https://fdev.soleapp.com.au/",
-    };
-
-    const supportParams = {
-      userName: "Ravi Joshi",
-      supportEmail: "test@gmail.com",
-      supportEndDate: "25/08/2025",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-      appStoreLink: "https://www.apple.com/in/app-store/",
-      playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
-      loginLink: "https://fdev.soleapp.com.au/",
-      regardsBy: "Neeraj Verma",
-    };
-
-    const quoteParams = {
-      userName: "Ashish Rautela",
-      quoteAmt: "AUD $225.00",
-      quoteId: "22222222",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-      appStoreLink: "https://www.apple.com/in/app-store/",
-      playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
-    };
-
-    const verificationParams = {
-      userName: "Ravi Joshi",
-      verifyAccountLink: "https://fdev.soleapp.com.au/",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-      appStoreLink: "https://www.apple.com/in/app-store/",
-      playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
-    };
-
-    const otpParams = {
-      userName: "Ashish Rautela",
-      otp: "123456",
-      twitterLink: "https://x.com/home",
-      linkedinLink: "https://www.linkedin.com/",
-      facebookLink: "https://www.facebook.com/",
-      helpcenterLink: "https://fdev.soleapp.com.au/",
-      privacyLink: "https://fdev.soleapp.com.au/",
-      termsLink: "https://fdev.soleapp.com.au/",
-      appStoreLink: "https://www.apple.com/in/app-store/",
-      playStoreLink: "https://play.google.com/store/games?hl=en_IN&pli=1",
-    };
-
     // await resend.emails.send({
     //   from: "PaidEarly <support@paidearly.in>",
     //   to: adminEmail,
@@ -148,11 +177,25 @@ export const sendCompanyCreationEmail = async (adminEmail) => {
     //   subject: "Welcome to Sole",
     //   html: verification(verificationParams),
     // });
+    // await resend.emails.send({
+    //   from: "PaidEarly <support@paidearly.in>",
+    //   to: adminEmail,
+    //   subject: "Welcome to Sole",
+    //   html: otp(otpParams),
+    // });
+
+    const invoiceReminderBase64 = await pdfBase64();
     await resend.emails.send({
       from: "PaidEarly <support@paidearly.in>",
       to: adminEmail,
       subject: "Welcome to Sole",
-      html: otp(otpParams),
+      html: invoiceReminder(invoiceReminderParams),
+      attachments: [
+        {
+          filename: "INV-02.pdf",
+          content: invoiceReminderBase64,
+        },
+      ],
     });
     console.log(`Email sent successfully to ${adminEmail}`);
   } catch (error) {
